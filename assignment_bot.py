883fccef9355a2d3b374a678cf93828aab9dc462
Paul-Messina <paul.messina16@gmail.com>
2021-02-23 11:46:31 -0500

    added assignment_bot.py which is being used in repl for hosting

--- assignment_bot.py
+++ assignment_bot.py
@@ -1,43 +1,37 @@
 import slack
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 # Import Flask
 from flask import Flask
 # Handles events from Slack
 from slackeventsapi import SlackEventAdapter
 
-# Load the Token from .env file
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
-
 # Configure your flask application
 app = Flask(__name__)
 
 # Configure SlackEventAdapter to handle events
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], '/slack/events', app)
+slack_event_adapter = SlackEventAdapter(os.getenv('SIGNING_SECRET'),'/slack/events',app)
 
 # Using WebClient in slack, there are other clients built-in as well !!
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.getenv('SLACK_TOKEN'))
 
 # connect the bot to the channel in Slack Channel
-client.chat_postMessage(channel='#bot-test-channel', text='Bot 1 Test')
+# client.chat_postMessage(channel='#bot-test-channel', text='Assignment Bot Test')
 
 # Get Bot ID
 BOT_ID = client.api_call("auth.test")['user_id']
-
 
 # handling Message Events
 @slack_event_adapter.on('message')
 def message(payload):
     print(payload)
-    event = payload.get('event', {})
+    event = payload.get('event',{})
     channel_id = event.get('channel')
     user_id = event.get('user')
-    text2 = event.get('text')
-    if BOT_ID !=user_id:
-        client.chat_postMessage(channel=channel_id, text=text2)
+    question = event.get('text')
+    if BOT_ID != user_id:
+      if '?' in question:
+        client.chat_postMessage(channel=channel_id, text=question)
 
 # Run the webserver micro-service
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0',port=5000)
