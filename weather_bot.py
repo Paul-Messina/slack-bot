c23387d163cdb1f53124cabd2c4894ab722f527a
Paul-Messina <paul.messina16@gmail.com>
2021-02-28 17:09:04 -0500

    fixed os.getenv to os.environ to work on local machines

--- weather_bot.py
+++ weather_bot.py
@@ -11,9 +11,9 @@ load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
 
-slack_event_adapter = SlackEventAdapter(os.getenv('SIGNING_SECRET'),'/slack/events',app)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
 
-slack_client = slack.WebClient(token=os.getenv('SLACK_TOKEN'))
+slack_client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 
 BOT_ID = slack_client.api_call("auth.test")['user_id']
 
