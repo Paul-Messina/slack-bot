275184e021df889e0bc01629e3a09dfc2baff6fc
Briltz <jake.briltz12@gmail.com>
2021-04-09 22:32:50 -0400

    !!!

--- test_calculator.py
+++ test_calculator.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_add(self):
         resultAdd = calculator.add_func(10, 5)
-        self.assertEqual(resultAdd, 16)
+        self.assertEqual(resultAdd, 15)
     
     def test_subtract(self):
         resultSub = calculator.subtract_func(10, 5)
